4d488ba6caf091355da82eb71ad40b4fbca62009
doleksiyenko <dan.oleksiyenko@gmail.com>
2020-06-20 11:32:18 -0400

    Generate a random length 15 code to join room

--- public/js/welcome.js
+++ public/js/welcome.js
@@ -1,1 +1,21 @@
-const joinRoomByRoomForm = document.getElementById("join-room-id");
+const generateIdButton = document.getElementById("generate-room-id");
+const input = document.querySelector("input");
+const span = document.querySelector("span");
+
+generateIdButton.onclick = (e) => {
+    e.preventDefault();
+    let randomRoomId = makeId(15);
+    input.value = randomRoomId;
+    span.textContent = `Click Join/Create Room to join room. Send this code to others for them to join the room : ${randomRoomId}`;
+};
+
+makeId = (length) => {
+    let result = "";
+    let characters =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let characterNum = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characterNum));
+    }
+    return result;
+};
